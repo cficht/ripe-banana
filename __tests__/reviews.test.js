dd92677c1009f4748450416a23a09da814667366
cficht <chris.ficht@gmail.com>
2020-03-27 13:38:34 -0700

    tested and created review post route

---
+++ __tests__/reviews.test.js
@@ -0,0 +1,30 @@
+require('../db/data-helpers');
+const mongoose = require('mongoose');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('reviews routes', () => {
+
+  it('creates a review', () => {
+    return request(app)
+      .post('/api/v1/reviews')
+      .send({
+        rating: 4,
+        reviewer: new mongoose.Types.ObjectId(),
+        review: 'It was pretty good. Could be better, but I enjoyed it.',
+        film: new mongoose.Types.ObjectId()
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          rating: 4,
+          reviewer: expect.any(String),
+          review: 'It was pretty good. Could be better, but I enjoyed it.',
+          film: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+});
