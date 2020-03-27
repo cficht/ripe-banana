635ecfdd9eca107136a713e71ba2984f06e71fdf
cficht <chris.ficht@gmail.com>
2020-03-27 16:51:17 -0700

    fixed get actors by id route to also get films

--- __tests__/actors.test.js
+++ __tests__/actors.test.js
@@ -1,10 +1,9 @@
-const { getActor, getActors } = require('../db/data-helpers');
+const { getActor, getActors, getFilms } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
 
 describe('actors routes', () => {
-
   it('creates an actor', () => {
     return request(app)
       .post('/api/v1/actors')
@@ -26,7 +25,6 @@ describe('actors routes', () => {
 
   it('gets all actors', async() => {
     const actors = await getActors();
-
     return request(app)
       .get('/api/v1/actors')
       .then(res => {
@@ -41,11 +39,24 @@ describe('actors routes', () => {
 
   it('gets an actor by id', async() => {
     const actor = await getActor();
+    const films = await getFilms();
+    let actorFilms = [];
+    films.forEach(film => {
+      film.cast.forEach(member => {
+        if(member.actor === actor._id) {
+          delete film.cast;
+          delete film.studio;
+          delete film.__v;
+          actorFilms.push(film);
+        }
+      });
+    });
 
     return request(app)
       .get(`/api/v1/actors/${actor._id}`)
       .then(res => {
-        expect(res.body).toEqual(actor);
+        delete actor.__v;
+        expect(res.body).toEqual({ ...actor, films: actorFilms });
       });
   });
 });
